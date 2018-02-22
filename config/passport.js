436eb34399db092fb2b33a37af2c99d008b60214
Frekky <sobolrip1@gmail.com>
2018-02-22 11:50:39 +0200

    Validation email and password

--- config/passport.js
+++ config/passport.js
@@ -17,6 +17,16 @@ passport.use('local.signup', new LocalStrategy({
     passwordField: 'password',
     passReqToCallback: true
 }, (req, email, password, done) => {
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty().isLength({min: 4});
+    const errors = req.validationErrors();
+    if(errors) {
+        let messages = [];
+        errors.forEach(error => {
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
     User.findOne({'email': email}, (err,user) => {
         if(err) {
             return done(err);
@@ -24,7 +34,7 @@ passport.use('local.signup', new LocalStrategy({
         if(user) {
             return done(null, false, {message: 'Email is already in use.'});
         }
-        let newUser = new User();
+        const newUser = new User();
         newUser.email = email;
         newUser.password = newUser.encryptPassword(password);
         newUser.save((err, result) => {
